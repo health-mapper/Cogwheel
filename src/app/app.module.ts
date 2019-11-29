d365f5c0d65098607505a5acd070eae77e28e067
Vadim Peretokin <vperetokin@gmail.com>
2019-11-29 17:50:04 +0100

    Add the input file component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogicalModelsComponent } from './logical-models/logical-models.component';
 import { MappingEditorComponent } from './mapping-editor/mapping-editor.component';
+import { InputFileComponent } from './input-file/input-file.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogicalModelsComponent,
-    MappingEditorComponent
+    MappingEditorComponent,
+    InputFileComponent
   ],
   imports: [
     BrowserModule,
